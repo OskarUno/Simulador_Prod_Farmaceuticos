172240c13d5bcb2e1bd9fe0f365eed5ac720a955
Oskar_Uno <oskar310cl@gmail.com>
2024-12-02 23:27:32 -0300

    Se crean las clases en Laboratorio, se modifica el archivo Setting.py, se registra el modelo en admin

--- A_Simulador_Prod_Farmaceuticos/laboratorio/models.py
+++ A_Simulador_Prod_Farmaceuticos/laboratorio/models.py
@@ -1,3 +1,32 @@
+import datetime
 from django.db import models
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+class Laboratorio(models.Model):
+    nombre = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.nombre
+    
+class DirectorGeneral(models.Model):
+    nombre = models.CharField(max_length=100)
+    laboratorio = models.OneToOneField(Laboratorio, max_length=100, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.nombre} -- {self.laboratorio}"
+    
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    laboratorio = models.ForeignKey(Laboratorio, on_delete=models.CASCADE)
+    f_fabricacion = models.DateField(validators=[MinValueValidator(datetime.date(2015,1,1))])
+    p_costo = models.DecimalField(max_digits=10, decimal_places=2)
+    p_venta = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    def save(self, *args, **kwargs):
+        # Validar los campos de la tabla antes de Guardar
+        self.full_clean()
+        super().save(*args,**kwargs)
+    
+    def __str__(self):
+        return f'{self.nombre} - {self.laboratorio}'
+    
