af775bfcc5bb05fc30661a7c7aa9800919ddc6a6
Oskar_Uno <oskar310cl@gmail.com>
2024-12-03 00:48:37 -0300

    Se actualizaron clases Laboratorio y DirectorGeneral

--- A_Simulador_Prod_Farmaceuticos/laboratorio/admin.py
+++ A_Simulador_Prod_Farmaceuticos/laboratorio/admin.py
@@ -2,20 +2,21 @@ from django.contrib import admin
 from .models import DirectorGeneral, Laboratorio, Producto
 
 # Registros de los modelos en el admin
-admin.site.register(Laboratorio)
+@admin.register(Laboratorio)
 class LaboratorioAdmin(admin.ModelAdmin):
     list_display = ('id','nombre',)     # Define las columnas visibles en la lista
     search_fields = ('nombre',)         # Define los campos que se pueden buscar.
     ordering = ('id',)                  # Define el orden de los registros.
 
-admin.site.register(DirectorGeneral)
+@admin.register(DirectorGeneral)
 class DirectorGeneralAdmin(admin.ModelAdmin):
     list_display = ('id','nombre', 'laboratorio')
     search_fields = ('nombre',)
     ordering = ('id',)
 
-admin.site.register(Producto)
+@admin.register(Producto)
 class ProductoAdmin(admin.ModelAdmin):
     list_display = ('id','nombre', 'laboratorio', 'f_fabricacion', 'p_costo', 'p_venta')
     search_fields = ('nombre',)
     ordering = ('id',)
+    list_filter = ('nombre', 'laboratorio')   # Filtros por nombre
--- A_Simulador_Prod_Farmaceuticos/laboratorio/models.py
+++ A_Simulador_Prod_Farmaceuticos/laboratorio/models.py
@@ -4,6 +4,8 @@ from django.core.validators import MinValueValidator
 
 class Laboratorio(models.Model):
     nombre = models.CharField(max_length=50)
+    ciudad = models.CharField(max_length=100, null=True)
+    pais   = models.CharField(max_length=50, null=True)
     
     def __str__(self):
         return self.nombre
@@ -11,6 +13,7 @@ class Laboratorio(models.Model):
 class DirectorGeneral(models.Model):
     nombre = models.CharField(max_length=100)
     laboratorio = models.OneToOneField(Laboratorio, max_length=100, on_delete=models.CASCADE)
+    especialidad = models.CharField(max_length=200, null=True)
     
     def __str__(self):
         return f"{self.nombre} -- {self.laboratorio}"
